10023d2a58cb398d21d83468eef2956da5abb27a
Michał Sznajder <michalsznajder@gmail.com>
2021-07-11 18:51:33 +0200

    initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication.Pages
+{
+
+    public class ComplexType
+    {
+        // [ValidateNever]
+        public Maybe<string> MaybeField { get; set; }
+    }
+    
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public ComplexType ComplexType { get; set; }
+        
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage();
+        }
+    }
+}
